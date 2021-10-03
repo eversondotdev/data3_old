e3d14565a2f7f4a448db905f1422908c405e10af
Everson Carvalho <eversoncarvalho@Eversons-MacBook-Air.local>
2021-10-03 17:28:10 -0300

    test deploy

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const pool = require('./db')
+// const pool = require('./db')
 
 app.use(express.json())
 
@@ -8,30 +8,30 @@ app.get('/', function(req, res){
     res.send('I think its working')
 })
 
-//All companies profiles
-app.get('/profiles', async(req, res) => {
-    try {
-        const companies = await pool.query('SELECT * FROM profiles')
+// //All companies profiles
+// app.get('/profiles', async(req, res) => {
+//     try {
+//         const companies = await pool.query('SELECT * FROM profiles')
 
-        res.json(companies.rows)
-    } catch (err) {
-        console.log(err.message)
-    }
-})
+//         res.json(companies.rows)
+//     } catch (err) {
+//         console.log(err.message)
+//     }
+// })
 
-//A single company profile
-app.get('/profiles/:id', async (req, res) => {
-    const { id } = req.params;
-    try {
-        const comapany = await pool.query('SELECT * from profiles WHERE id = $1', [
-            id
-        ])
+// //A single company profile
+// app.get('/profiles/:id', async (req, res) => {
+//     const { id } = req.params;
+//     try {
+//         const comapany = await pool.query('SELECT * from profiles WHERE id = $1', [
+//             id
+//         ])
 
-        res.json(comapany.rows[0])
-    } catch (err) {
-        console.log(err.message)
-    }
-})
+//         res.json(comapany.rows[0])
+//     } catch (err) {
+//         console.log(err.message)
+//     }
+// })
 
 const port = process.env.PORT || 5000
 
