131076b2826bc85ee4b8e88dcb48bd461f39aaa6
Everson Carvalho <eversoncarvalho@Eversons-MacBook-Air.local>
2021-10-26 16:52:05 -0300

    deploy heroku

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const Pool = require("pg").Pool
+require('dotenv').config();
 
-const pool = new Pool({
-    user: "postgres",
-    password: "42d4t4XY!",
-    database:"data3_local",
-    host:"localhost",
-    port: 5432
-})
+const Config = {
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT
+}
+
+const pool = new Pool(Config)
 
 module.exports = pool
