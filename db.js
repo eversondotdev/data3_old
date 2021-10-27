5264b0481a8b1f2c59c0133f70158b68cbc93268
Everson Carvalho <eversoncarvalho@Eversons-MacBook-Air.local>
2021-10-27 17:43:44 -0300

    prod pool

--- db.js
+++ db.js
@@ -1,13 +1,23 @@
 const Pool = require("pg").Pool
-require('dotenv').config()
+require("dotenv").config();
 
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const devConfig = {
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT
+}
 
-const proConfig = process.env.DATABASE_URL; //heroku addons
+const prodConfig = {
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME,
+    host: process.env.RDS_HOSTNAME,
+    port: process.env.RDS_PORT
+}
 
-const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === "production" ? proConfig : devConfig,
-});
+//DEFINE PATHS TO PROD AND DEV
+const pool = new Pool(prodConfig)
 
 module.exports = pool
